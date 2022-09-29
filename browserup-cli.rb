264d65624183e402771611d7b67bcc0f5a4e2052
HereC <ebeland@gmail.com>
2022-09-28 20:06:33 -0400

    rename to fix

--- browserup-cli.rb
+++ browserup-cli.rb
@@ -1,7 +1,9 @@
+require 'formula'
+
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class HomebrewBrowserupCli < Formula
+class BrowserupCli < Formula
   desc "Browserup CLI Application For Load Testing"
   homepage "https://browserup.com"
   url "https://github.com/browserup/homebrew-browserup-cli/archive/refs/tags/0.0.0.tar.gz"
